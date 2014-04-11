bd7c851320a3f5afd830d6d0854698b21529a68c
Stanislav <verve111@mail.ru>
2014-04-11 22:02:26 +0200

    test commit

--- step-trace/src/tests/AllTests.java
+++ step-trace/src/tests/AllTests.java
@@ -7,5 +7,5 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(Suite.class)
 @SuiteClasses({ NonRotationalCubic.class, NonRotationalFlat.class, NonRotationalLong.class, Rotational.class })
 public class AllTests {
-	//
+	///
 } 
